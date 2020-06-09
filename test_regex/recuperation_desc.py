b37f6a215d1733062fd34ea8256c1eb7a3c20ca5
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2020-06-09 23:30:36 +0200

    working on regex test script

--- test_regex/recuperation_desc.py
+++ test_regex/recuperation_desc.py
@@ -14,28 +14,44 @@ def desc_extractor(input):
         root = f.getroot()
         desc = root.xpath("//tei:desc", namespaces=tei)
         for i in desc:
-            dernier_item = i.text.split(" ")[-1]
+            i = remove_multiple_spaces(i.text)
+            dernier_item = re.split("[\s]", i)[-1]
             pattern = re.compile("[0-999]")
             if pattern.match(dernier_item):
                 trigger_price()
+            else:
+                with open("errs.txt", "a") as err_log:
+                    err_log.write("%s (%s)\n" % (dernier_item,i))
         return desc
+
+
+def remove_multiple_spaces(text):
+    text = re.sub('	', ' ', text)
+    text = re.sub('\n', ' ', text)
+    text = re.sub('\s+', ' ', text)
+    return text
 
 
 def trigger_price():
     global n
     n += 1
 
+
 def total_number():
     global p
     p += 1
 
+
 if __name__ == "__main__":
     n = 0
     p = 0
+    file = open("errs.txt", "r+")
+    file.truncate(0)
+    file.close()
     with open("desc.txt", "w") as output:
         for i in glob.iglob('../../Data/*.xml'):
             for j in desc_extractor(i):
                 total_number()
-                output.write("%s\n" % j.text.replace('\n', ' '))
+                output.write("%s\n" % j.text.replace("\n", "s"))
     print("Items with price: %s" % n)
     print("Total numbers: %s" % p)
